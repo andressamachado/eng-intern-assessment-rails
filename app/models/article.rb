b854c1f47c5ce21bf50b103aaea06ab58f9416a9
andressamachado <andressa.pamachado@gmail.com>
2024-01-17 16:37:57 -0500

    Add method create to Article model.

--- app/models/article.rb
+++ app/models/article.rb
@@ -7,5 +7,14 @@ class Article < ApplicationRecord
   # Ensure Title is unique per Author.
   validates :title, uniqueness: { scope: :author, message: 'must be unique per author' }
 
+  # Create a new Article instance.
+  def self.create(title:, content:, author: 'Anonymous', date: Date.today)
+    article = Article.new(author: author, title: title, content: content, date: date)
+    article.save
 
+    return article
+  end
+
+  # TODO: Search for articles by Title or Content.
+  def self.search(term) end
 end
