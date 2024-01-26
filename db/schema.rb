8b0018592a548af99c736633ac3a331caf933173
andressamachado <andressa.pamachado@gmail.com>
2024-01-26 10:57:34 -0500

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2024_01_24_174747) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "articles", force: :cascade do |t|
     t.string "author"
     t.string "title", null: false
@@ -23,7 +26,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_24_174747) do
   create_table "comments", force: :cascade do |t|
     t.text "username"
     t.text "body"
-    t.integer "article_id", null: false
+    t.bigint "article_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["article_id"], name: "index_comments_on_article_id"
