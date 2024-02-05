ca64229a30be5ba8098151c90cae8c4b99bcb109
andressamachado <andressa.pamachado@gmail.com>
2024-02-05 02:12:29 -0500

    Add user migration, fixture, and schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_24_174747) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_03_233148) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,7 +18,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_24_174747) do
     t.string "author"
     t.string "title", null: false
     t.string "content", null: false
-    t.date "date", default: "2024-01-25"
+    t.date "date", default: "2024-02-05"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -30,6 +30,16 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_24_174747) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username", null: false
+    t.string "email", null: false
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "comments", "articles"
