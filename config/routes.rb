51630080772e975ef88fcaaec979327b6a95d8c9
andressamachado <andressa.pamachado@gmail.com>
2024-02-05 03:37:51 -0500

    Add route to logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,21 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get "comments/create"
-
   # Defines the root path route ("/")
   root "home#index"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  get "comments/create"
+
   resources :articles do
     resources :comments, only: [:edit, :create, :destroy, :update]
   end
 
   get "sign_up", to: "registrations#new"
   post "sign_up", to: "registrations#create"
+
+  delete "logout", to: "sessions#destroy"
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
